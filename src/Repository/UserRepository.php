f4ee17554c7333a80631f918e9706bf07cbbd7c7
Billy Purvis <billy@placed-app.com>
2018-02-13 22:17:44 +0000

    Adds new use statements and uses entity manager in the correct fashion for saving users.

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,12 +4,13 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
-
 
 /**
  * Class UserRepository
@@ -19,17 +20,26 @@ use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
  */
 class UserRepository extends EntityRepository implements UserLoaderInterface
 {
-//    public function findBySomething($value)
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->where('u.something = :value')->setParameter('value', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * @var EntityManager|EntityManagerInterface
+     */
+    private $em;
 
+    /**
+     * UserRepository constructor.
+     * @param EntityManager|EntityManagerInterface $em
+     * @param ORM\ClassMetadata $class
+     */
+    public function __construct($em, ORM\ClassMetadata $class)
+    {
+        parent::__construct($em, $class);
+        $this->em = $em;
+    }
+
+    /**
+     * @param string $username
+     * @return mixed
+     */
     public function loadUserByUsername($username)
     {
         return $this->createQueryBuilder('u')
@@ -40,10 +50,14 @@ class UserRepository extends EntityRepository implements UserLoaderInterface
             ->getOneOrNullResult();
     }
 
-    public function save(User $user, EntityManager $em) {
-        $em->persist($user);
-        $em->flush($user);
-        $em->clear($user);
+    /**
+     * @param User $user
+     */
+    public function save(User $user)
+    {
+        $this->em->persist($user);
+        $this->em->flush($user);
+        $this->em->clear(User::class);
         return;
     }
 }
